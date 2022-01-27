fb777bd6e0a86d27fd59e64a19fbf54673344f13
Onur <58780366+onurnafiguzel@users.noreply.github.com>
2022-01-27 21:38:31 +0300

    UnitOfWork

---
+++ AuthServer.Data/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using AuthServer.Core.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public readonly AppDbContext _appDbContext;
+
+        public UnitOfWork(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public void Commit()
+        {
+            _appDbContext.SaveChanges();
+        }
+
+        public async Task CommmitAsync()
+        {
+            await _appDbContext.SaveChangesAsync();
+        }
+    }
+}
